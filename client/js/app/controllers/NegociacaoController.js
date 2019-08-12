f72396f909c691d24dda4f23060d2c49ee45c700
Thiago Silva <thiago.silva@hotmail.com>
2019-08-12 15:32:27 -0300

    Resolucao do problema com o formato de datas. adicionado o replace para alterar o texto

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,20 +1,27 @@
 class NegociacaoController {
 
 	constructor(){
-
+		// |> bind para o this apontar para document
 		let $ = document.querySelector.bind(document)
-		this.inputData = $('#data')
-		this.inputQuantidade = $('#quantidade')
-		this.inputValor = $('#valor')
+		this._inputData = $('#data')
+		this._inputQuantidade = $('#quantidade')
+		this._inputValor = $('#valor')
 	}
 
 	//Criar metodo que recebera um evento
 	adiciona(event){
 		event.preventDefault()
-		// |> bind para o this apontar para document
+		console.log(this._inputData.value)
+		//converte o horario para tipo date.
+		//let data = new Date(this._inputData.value.split('-'))
+		let data = new Date(this._inputData.value.replace(/-/g, ','))
+		let negociacao = new Negociacao(
+			data,
+			this._inputQuantidade.value,
+			this._inputValor.value
+		)
 
-
-
-		console.log(`${this.inputData.value} ${this.inputQuantidade.value} ${this.inputValor.value}`)
+		// ==> adicionar a negociação em uma lista
+		console.log(negociacao)
 	}
 }
--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,6 +1,6 @@
 class Negociacao{
 	constructor(data, quantidade, valor){
-		this._data = new Date(data)
+		this._data = new Date(data.getTime())
 		this._quantidade = quantidade
 		this._valor = valor
 

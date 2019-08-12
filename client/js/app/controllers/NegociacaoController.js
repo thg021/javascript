729d67d17f9a7de1fcb8630addb7b1630ae4302b
Thiago Silva <thiago.silva@hotmail.com>
2019-08-12 14:00:00 -0300

    Adicionado o bind para apontamento para o document

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -3,6 +3,16 @@ class NegociacaoController {
 	//Criar metodo que recebera um evento
 	adiciona(event){
 		event.preventDefault()
-		alert('Chamei aqui')
+		// |> bind para o this apontar para document
+
+		let $ = document.querySelector.bind(document)
+
+		console.log($)
+
+		let inputData = $('#data')
+		let inputQuantidade = $('#quantidade')
+		let inputValor = $('#valor')
+
+		console.log(`${inputData.value} ${inputQuantidade.value} ${inputValor.value}`)
 	}
 }

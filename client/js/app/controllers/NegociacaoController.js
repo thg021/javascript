05e613d735bfc3f4f2c871acc1c2ec781350234c
Thiago Silva <thiago.silva@hotmail.com>
2019-08-12 15:17:53 -0300

    Adicionando o constructor para melhorar o desempenho da aplicação e evitar acessos desnecessarios na DOM

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,18 +1,20 @@
 class NegociacaoController {
 
+	constructor(){
+
+		let $ = document.querySelector.bind(document)
+		this.inputData = $('#data')
+		this.inputQuantidade = $('#quantidade')
+		this.inputValor = $('#valor')
+	}
+
 	//Criar metodo que recebera um evento
 	adiciona(event){
 		event.preventDefault()
 		// |> bind para o this apontar para document
 
-		let $ = document.querySelector.bind(document)
 
-		console.log($)
 
-		let inputData = $('#data')
-		let inputQuantidade = $('#quantidade')
-		let inputValor = $('#valor')
-
-		console.log(`${inputData.value} ${inputQuantidade.value} ${inputValor.value}`)
+		console.log(`${this.inputData.value} ${this.inputQuantidade.value} ${this.inputValor.value}`)
 	}
 }
